eedae5aa0ca4c726abc4bccb6a5f6a74326220e4
RUDRANSHFLY <mayamayavir@gmail.com>
2025-04-01 10:06:26 +0530

    Refactor layout component and add index file for YogaList

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import YogaList from "@/components/YogaList";
+import { Text, View } from "react-native";
+
+export default function Index(){
+    return (
+        <View className="mt-5">
+            <YogaList />
+        </View>        
+    )
+}
